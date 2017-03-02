fb4dc95aa87dfd8de9dd3e0df33820d45eff2213
Rob <phys.rr@gmail.com>
2017-03-02 10:55:25 +0000

    Remove stale public folder

--- setup.sh
+++ setup.sh
@@ -1,40 +1,46 @@
 #!/usr/bin/env bash
 
-# This script allows you to easily and quickly generate and deploy your website
-# using Hugo to your personal GitHub Pages repository. This script requires a
-# certain configuration, run the `setup.sh` script to configure this. See
+# This script does the required work to set up your personal GitHub Pages
+# repository for deployment using Hugo. Run this script only once -- when the
+# setup has been done, run the `deploy.sh` script to deploy changes and update
+# your website. See
 # https://hjdskes.github.io/blog/deploying-hugo-on-personal-github-pages/index.html
 # for more information.
 
-# Set the English locale for the `date` command.
-export LC_TIME=en_US.UTF-8
-
-# GitHub username.
+# GitHub username
 USERNAME=mlnotebook
 # Name of the branch containing the Hugo source files.
 SOURCE=hugo
-# The commit message.
-MESSAGE="Site rebuild $(date)"
-if [ $# -eq 1 ]
-	then MESSAGE="$1"
-fi
 
 msg() {
     printf "\033[1;32m :: %s\n\033[0m" "$1"
 }
 
-msg "Pulling down the \`master\` branch into \`public\` to help avoid merge conflicts"
+msg "Deleting the \`master\` branch"
+git branch -D master
+git push origin --delete master
+
+msg "Creating an empty, orphaned \`master\` branch"
+git checkout --orphan master
+git rm --cached $(git ls-files)
+
+msg "Grabbing one file from the \`$SOURCE\` branch so that a commit can be made"
+git checkout "$SOURCE" README.md
+git commit -m "Initial commit on master branch"
+git push origin master
+
+msg "Returning to the \`$SOURCE\` branch"
+git checkout -f "$SOURCE"
+
+msg "Removing the \`public\` folder to make room for the \`master\` subtree"
+rm -rf public
+git add -u
+git commit -m "Remove stale public folder"
+
+msg "Adding the new \`master\` branch as a subtree"
+git subtree add --prefix=public \
+	git@github.com:$USERNAME/$USERNAME.github.io.git master --squash
+
+msg "Pulling down the just committed file to help avoid merge conflicts"
 git subtree pull --prefix=public \
-	git@github.com:$USERNAME/$USERNAME.github.io.git master -m "Merge master"
-
-msg "Building the website"
-hugo
-
-msg "Pushing the updated \`public\` folder to the \`$SOURCE\` branch"
-git add public
-git commit -m "$MESSAGE"
-git push origin "$SOURCE"
-
-msg "Pushing the updated \`public\` folder to the \`master\` branch"
-git subtree push --prefix=public \
 	git@github.com:$USERNAME/$USERNAME.github.io.git master
